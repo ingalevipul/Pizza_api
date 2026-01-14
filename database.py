387b9ebc8c04b729d19e777d8b8ad7cd40d1efb9
vipul <vipul.ingale147@gmail.com>
2026-01-14 16:38:35 +0530

    jwt_Added

--- database.py
+++ database.py
@@ -1,11 +1,15 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL ="postgresql://postgres:vipul@localhost:5432/pizza_api_db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL,echo=True)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
